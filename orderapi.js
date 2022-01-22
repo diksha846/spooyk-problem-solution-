bbbcb633df06bf178209b4bde57a585641cc92c5
diksha846 <96564391+diksha846@users.noreply.github.com>
2022-01-22 16:19:45 +0530

    Update orderapi.js

--- orderapi.js
+++ orderapi.js
@@ -4,4 +4,34 @@ OrderNumber
 	CustomerId
 	DeliveryVehicleId
 	IsDelivered 
+	// Importing express module
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+const { ItemId,
+	Price,
+	CustomerId,
+	DeliveryVehicleId,
+	IsDelivered } = req.body;
+const { authorization } = req.headers;
+res.send({
+	ItemId,
+	Price,
+	CustomerId,
+	DeliveryVehicleId,
+	IsDelivered
+	
+});
+});
+
+app.listen(8081, () => {
+console.log('Our express server is up on port 8081');
+});
   
