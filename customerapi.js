c5a55de03289c9fb74fc07f4a756d1d2179db563
diksha846 <96564391+diksha846@users.noreply.github.com>
2022-01-22 16:11:29 +0530

    Update customerapi.js

--- customerapi.js
+++ customerapi.js
@@ -1,1 +1,27 @@
+// Importing express module
+const express = require('express');
+const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+const { RegistrationNumber,
+VehicleType,
+City,
+ActiveOrdersCount } = req.body;
+const { authorization } = req.headers;
+res.send({RegistrationNumber,
+	VehicleType,
+	City,
+	ActiveOrdersCount
+
+});
+});
+
+app.listen(8081, () => {
+console.log('Our express server is up on port 8081');
+});
